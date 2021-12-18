95189afdaf092705270b88ad6f6d522596459ebf
marvelyko <61324314+marvelyko@users.noreply.github.com>
2021-12-18 21:59:01 +0400

    m

--- map/static/scripts.js
+++ map/static/scripts.js
@@ -1,13 +1,33 @@
 var lat = 0;
 var lng = 0;
 
-function handleClick(cord){
-    var popup = document.getElementById('popup');
+var map = L.map("map").setView([41.719562, 44.788718], 8);
+L.tileLayer(
+  "https://api.maptiler.com/maps/streets/256/{z}/{x}/{y}.png?key=23gNtsMw6Lr7QuwmLLh6",
+  {
+    atributtion:
+      '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
+  }
+).addTo(map);
 
-    popup.style.display = 'block';
+map.on("click", function (event) {
+  handleClick(event.latlng);
+});
 
-    lat = cord.lat;
-    lng = cord.lng;
+function handleClick(cord) {
+  var popup = document.getElementById("popup");
 
-    console.log(lat,lng)
+  popup.style.display = "block";
+
+  lat = cord.lat;
+  lng = cord.lng;
+
+  console.log(lat, lng);
+}
+
+function popupClose() {
+  var close_popup = document.getElementById("close");
+  var popup = document.getElementById("popup");
+
+  popup.style.display = "none";
 }
