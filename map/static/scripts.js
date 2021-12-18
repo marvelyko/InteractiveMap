727dd49159675bd80e6cb48bdd1c863464166dc2
Tengiz pataraia <pataraiamistera@gmail.com>
2021-12-18 23:30:36 +0400

    d

--- map/static/scripts.js
+++ map/static/scripts.js
@@ -2,13 +2,9 @@ var lat = 0;
 var lng = 0;
 
 var map = L.map("map").setView([41.719562, 44.788718], 8);
-L.tileLayer(
-  "https://api.maptiler.com/maps/streets/256/{z}/{x}/{y}.png?key=23gNtsMw6Lr7QuwmLLh6",
-  {
-    atributtion:
-      '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
-  }
-).addTo(map);
+L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+	attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+}).addTo(map);
 
 L.control.scale({
   metric: true,
